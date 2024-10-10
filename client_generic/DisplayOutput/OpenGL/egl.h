4559cf48da41f2d1d98d4735fb3dea59372639b8
Pascal Jäger <pascal.jaeger@leimstift.de>
2024-10-10 16:31:52 +0200

    first wayland build that doesn't fail
    Signed-off-by: Pascal Jäger <pascal.jaeger@leimstift.de>

--- client_generic/DisplayOutput/OpenGL/egl.h
+++ client_generic/DisplayOutput/OpenGL/egl.h
@@ -7,7 +7,10 @@
 #error "DisplayGL.h included before egl.h!"
 #endif
 
-#include <X11/Xlib.h>
+#include <wayland-client.h>
+#include <wayland-egl.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
 #include <unistd.h>
 
 #ifndef LINUX_GNU
@@ -22,20 +25,21 @@ namespace	DisplayOutput
 
 class CWaylandGL : public CDisplayOutput
 {
-    Display     *m_pDisplay;
-    GLXContext  m_GlxContext;
-    Window      m_Window;
-    GLXWindow   m_GlxWindow;
-    bool        m_FullScreen;
-    int         m_VSync;
+    wl_display       *m_pDisplay;
+    wl_compositor    *m_Compositor;
+    wl_surface       *m_Surface;
+    wl_shell         *m_Shell;
+    wl_shell_surface *m_ShellSurface;
+    wl_egl_window    *m_EGLWindow;
+    EGLDisplay m_EGLDisplay;
+    EGLContext m_EGLContext;
+    EGLSurface m_EGLSurface;
+    bool              m_FullScreen;
 
     uint32	m_WidthFS;
     uint32	m_HeightFS;
 
     void    setFullScreen( bool enabled );
-    void    setWindowDecorations( bool enabled );
-    void    toggleVSync();
-    void    alwaysOnTop();
     void    checkClientMessages();
     //bool    checkResizeEvent( ResizeEvent &event);
 
@@ -45,13 +49,15 @@ class CWaylandGL : public CDisplayOutput
 
 			static const char *Description()	{	return "Wayland OpenGL display";	};
 
-            virtual bool	Initialize( const uint32 _width, const uint32 _height, const bool _bFullscreen );
-
-			//
+            virtual bool Initialize( const uint32 _width, const uint32 _height, const bool _bFullscreen );
 			virtual void Title( const std::string &_title );
 			virtual void Update();
 
 			void SwapBuffers();
+
+            // Add a setter for setting m_Compositor
+            void SetCompositor(wl_compositor *compositor) { m_Compositor = compositor; }
+            friend void registry_handle_global(void *data, struct wl_registry *registry, uint32_t name, const char *interface, uint32_t version);
 };
 
 typedef	CWaylandGL	CDisplayGL;
@@ -59,5 +65,4 @@ typedef	CWaylandGL	CDisplayGL;
 }
 
 #endif
-
 #endif
