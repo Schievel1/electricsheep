f2a7e9d69fdafe97ed393c0f77d986a75e068fcb
Pascal Jäger <pascal.jaeger@leimstift.de>
2024-10-16 16:46:17 +0200

    cleanup
    Signed-off-by: Pascal Jäger <pascal.jaeger@leimstift.de>

--- client_generic/DisplayOutput/OpenGL/egl.cpp
+++ client_generic/DisplayOutput/OpenGL/egl.cpp
@@ -16,151 +16,6 @@
 
 namespace DisplayOutput {
 
-#ifdef HAVE_LIBDECOR
-void frame_configure(struct libdecor_frame *frame,
-                     struct libdecor_configuration *configuration,
-                     void *user_data) {
-  CWaylandGL *waylandGL = static_cast<CWaylandGL *>(user_data);
-  struct libdecor_state *state;
-  int width, height;
-
-  if (!libdecor_configuration_get_content_size(configuration, frame, &width,
-                                               &height)) {
-    width = waylandGL->floating_width;
-    height = waylandGL->floating_height;
-  }
-
-  waylandGL->content_width = width;
-  waylandGL->content_height = height;
-
-  wl_egl_window_resize(waylandGL->m_EGLWindow, waylandGL->content_width,
-                       waylandGL->content_height, 0, 0);
-  glViewport(0, 0, waylandGL->content_width, waylandGL->content_height);
-
-  state = libdecor_state_new(width, height);
-  libdecor_frame_commit(frame, state, configuration);
-  libdecor_state_free(state);
-
-  /* store floating dimensions */
-  if (libdecor_frame_is_floating(waylandGL->frame)) {
-    waylandGL->floating_width = width;
-    waylandGL->floating_height = height;
-  }
-
-  waylandGL->configured = true;
-}
-
-void frame_close(struct libdecor_frame *frame, void *user_data) {
-  CWaylandGL *waylandGL = static_cast<CWaylandGL *>(user_data);
-
-  waylandGL->m_bClosed = true;
-}
-
-void frame_commit(struct libdecor_frame *frame, void *user_data) {
-  CWaylandGL *waylandGL = static_cast<CWaylandGL *>(user_data);
-  eglSwapBuffers(waylandGL->m_EGLDisplay, waylandGL->m_EGLSurface);
-}
-
-static struct libdecor_frame_interface frame_interface = {
-    frame_configure,
-    frame_close,
-    frame_commit,
-};
-
-static void libdecor_error(struct libdecor *context, enum libdecor_error error,
-                           const char *message) {
-  fprintf(stderr, "Caught error (%d): %s\n", error, message);
-  exit(EXIT_FAILURE);
-}
-
-static struct libdecor_interface libdecor_interface = {
-    libdecor_error,
-};
-#endif
-
-static void xdg_wm_base_ping(void *data, struct xdg_wm_base *xdg_wm_base,
-                             uint32_t serial) {
-  xdg_wm_base_pong(xdg_wm_base, serial);
-}
-
-static const struct xdg_wm_base_listener xdg_wm_base_listener = {
-    .ping = xdg_wm_base_ping,
-};
-
-void registry_handle_global(void *data, struct wl_registry *registry,
-                            uint32_t name, const char *interface,
-                            uint32_t version) {
-  CWaylandGL *waylandGL = static_cast<CWaylandGL *>(data);
-  // fprintf(stderr, "Got a registry event for %s id %d\n", interface, name);
-  if (strcmp(interface, wl_compositor_interface.name) == 0) {
-    waylandGL->m_Compositor = (struct wl_compositor *)wl_registry_bind(
-        registry, name, &wl_compositor_interface, 1);
-  } else if (strcmp(interface, xdg_wm_base_interface.name) == 0) {
-    waylandGL->m_XdgWmBase = (struct xdg_wm_base *)wl_registry_bind(
-        registry, name, &xdg_wm_base_interface, 1);
-    xdg_wm_base_add_listener(waylandGL->m_XdgWmBase, &xdg_wm_base_listener,
-                             waylandGL);
-  } else if (strcmp(interface, wl_output_interface.name) == 0) {
-    waylandGL->m_Output = (struct wl_output *)wl_registry_bind(
-        registry, name, &wl_output_interface, 1);
-  } else if (strcmp(interface, zwlr_layer_shell_v1_interface.name) == 0) {
-    waylandGL->m_Shell = (struct zwlr_layer_shell_v1 *)wl_registry_bind(
-        registry, name, &zwlr_layer_shell_v1_interface, 1);
-  } else if (strcmp(interface, zxdg_decoration_manager_v1_interface.name) ==
-             0) {
-    waylandGL->m_DecorationManager =
-        (struct zxdg_decoration_manager_v1 *)wl_registry_bind(
-            registry, name, &zxdg_decoration_manager_v1_interface, 1);
-  }
-}
-
-static const struct wl_registry_listener registry_listener = {
-    registry_handle_global, nullptr};
-
-void xdg_toplevel_configure_handler(void *data,
-                                    struct xdg_toplevel *xdg_toplevel,
-                                    int32_t width, int32_t height,
-                                    struct wl_array *states) {
-  fprintf(stderr, "configure: %dx%d\n", width, height);
-  CWaylandGL *waylandGL = static_cast<CWaylandGL *>(data);
-  if (width == 0 || height == 0) {
-    /* Compositor is deferring to us */
-    return;
-  }
-  waylandGL->m_WidthFS = width;
-  waylandGL->m_HeightFS = height;
-  glViewport(0, 0, width, height);
-}
-
-void xdg_toplevel_close_handler(void *data, struct xdg_toplevel *xdg_toplevel) {
-  CWaylandGL *waylandGL = static_cast<CWaylandGL *>(data);
-  waylandGL->m_bClosed = true;
-}
-
-const struct xdg_toplevel_listener xdg_toplevel_listener = {
-    .configure = xdg_toplevel_configure_handler,
-    .close = xdg_toplevel_close_handler};
-
-void xdg_surface_configure_handler(void *data, struct xdg_surface *xdg_surface,
-                                   uint32_t serial) {
-  CWaylandGL *waylandGL = static_cast<CWaylandGL *>(data);
-  xdg_surface_ack_configure(xdg_surface, serial);
-  waylandGL->configured = true;
-}
-
-const struct xdg_surface_listener xdg_surface_listener = {
-    .configure = xdg_surface_configure_handler};
-
-void zwlr_layer_surface_configure_handler(
-    void *data, struct zwlr_layer_surface_v1 *zwlr_layer_surface_v1,
-    uint32_t serial, uint32_t width, uint32_t height) {
-  glViewport(0, 0, width, height);
-  zwlr_layer_surface_v1_ack_configure(zwlr_layer_surface_v1, serial);
-}
-
-const struct zwlr_layer_surface_v1_listener layer_surface_listener = {
-    .configure = zwlr_layer_surface_configure_handler};
-
 CWaylandGL::CWaylandGL() : CDisplayOutput() {
   // Constructor initialization
 }
@@ -168,8 +23,8 @@ CWaylandGL::CWaylandGL() : CDisplayOutput() {
 CWaylandGL::~CWaylandGL() {
   // Cleanup
 #ifdef HAVE_LIBDECOR
-  if (context) {
-    libdecor_unref(context);
+  if (m_LibdecorContext) {
+    libdecor_unref(m_LibdecorContext);
   }
 #endif
   if (m_EGLDisplay != EGL_NO_DISPLAY) {
@@ -188,9 +43,6 @@ CWaylandGL::~CWaylandGL() {
   }
   if (m_Surface) {
     wl_surface_destroy(m_Surface);
-  }
-  if (m_ShellSurface) {
-    wl_shell_surface_destroy(m_ShellSurface);
   }
   if (m_pDisplay) {
     wl_display_disconnect(m_pDisplay);
@@ -212,6 +64,7 @@ bool CWaylandGL::Initialize(const uint32 _width, const uint32 _height,
   assert(registry);
 
   // Add listeners to the registry to handle global events
+
   wl_registry_add_listener(registry, &registry_listener, this);
   // wl_display_dispatch(m_pDisplay);
   wl_display_roundtrip(m_pDisplay);
@@ -346,13 +199,14 @@ bool CWaylandGL::Initialize(const uint32 _width, const uint32 _height,
       fprintf(stderr, "Using libdecor\n");
       using_csd = true;
       configured = false;
-      floating_width = m_WidthFS;
-      floating_height = m_HeightFS;
-      context = libdecor_new(m_pDisplay, &libdecor_interface);
-      frame = libdecor_decorate(context, m_Surface, &frame_interface, this);
-      libdecor_frame_map(frame);
+      m_LibdecorFloatingWidth = m_WidthFS;
+      m_LibdecorFloatingHeight = m_HeightFS;
+      m_LibdecorContext = libdecor_new(m_pDisplay, &libdecor_interface);
+      m_LibdecorFrame = libdecor_decorate(m_LibdecorContext, m_Surface,
+                                          &frame_interface, this);
+      libdecor_frame_map(m_LibdecorFrame);
       while (!configured) {
-        if (libdecor_dispatch(context, 0) < 0) {
+        if (libdecor_dispatch(m_LibdecorContext, 0) < 0) {
           fprintf(stderr, "Failed to dispatch libdecor\n");
           return false;
         }
@@ -376,13 +230,13 @@ bool CWaylandGL::Initialize(const uint32 _width, const uint32 _height,
     }
 #endif
   } else { // wl_layer_shell
-    if (!m_Shell) {
+    if (!m_WlrLayerShell) {
       fprintf(stderr, "Compositor does not support layer shell\n");
       return false;
     }
     layer_surface = zwlr_layer_shell_v1_get_layer_surface(
-        m_Shell, m_Surface, m_Output, ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND,
-        "wallpaper");
+        m_WlrLayerShell, m_Surface, m_Output,
+        ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND, "wallpaper");
     zwlr_layer_surface_v1_set_size(layer_surface, 0, 0);
     zwlr_layer_surface_v1_set_anchor(layer_surface,
                                      ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP |
@@ -413,8 +267,8 @@ void CWaylandGL::Title(const std::string &_title) {
     }
 #ifdef HAVE_LIBDECOR
     else {
-      libdecor_frame_set_title(frame, _title.c_str());
-      libdecor_frame_set_app_id(frame, _title.c_str());
+      libdecor_frame_set_title(m_LibdecorFrame, _title.c_str());
+      libdecor_frame_set_app_id(m_LibdecorFrame, _title.c_str());
     }
 #endif
   }
@@ -427,14 +281,14 @@ void CWaylandGL::setFullScreen(bool enabled) {
     if (enabled) {
 #ifdef HAVE_LIBDECOR
       if (using_csd)
-        libdecor_frame_set_fullscreen(frame, m_Output);
+        libdecor_frame_set_fullscreen(m_LibdecorFrame, m_Output);
       else
 #endif
         xdg_toplevel_set_fullscreen(m_XdgToplevel, m_Output);
     } else {
 #ifdef HAVE_LIBDECOR
       if (using_csd)
-        libdecor_frame_unset_fullscreen(frame);
+        libdecor_frame_unset_fullscreen(m_LibdecorFrame);
       else
 #endif
         xdg_toplevel_unset_fullscreen(m_XdgToplevel);
@@ -447,7 +301,7 @@ void CWaylandGL::Update() { checkClientMessages(); }
 void CWaylandGL::SwapBuffers() {
 #ifdef HAVE_LIBDECOR
   if (using_csd) {
-    if (libdecor_dispatch(context, 0) < 0) {
+    if (libdecor_dispatch(m_LibdecorContext, 0) < 0) {
       fprintf(stderr, "libdecor dispatch failed");
     }
   }
--- client_generic/DisplayOutput/OpenGL/egl.h
+++ client_generic/DisplayOutput/OpenGL/egl.h
@@ -31,35 +31,38 @@
 namespace DisplayOutput {
 
 class CWaylandGL : public CDisplayOutput {
+  bool using_csd = false;
+  bool configured = false;
+  // wayland
   wl_display *m_pDisplay = nullptr;
   wl_compositor *m_Compositor = nullptr;
   wl_surface *m_Surface = nullptr;
-  zwlr_layer_shell_v1 *m_Shell = nullptr;
+  wl_output *m_Output = nullptr;
+  // xdg
   xdg_wm_base *m_XdgWmBase = nullptr;
   xdg_surface *m_XdgSurface = nullptr;
   xdg_toplevel *m_XdgToplevel = nullptr;
-  wl_shell_surface *m_ShellSurface = nullptr;
-  zwlr_layer_surface_v1 *layer_surface = nullptr;
-  wl_egl_window *m_EGLWindow = nullptr;
-  wl_output *m_Output = nullptr;
   zxdg_decoration_manager_v1 *m_DecorationManager = nullptr;
   zxdg_toplevel_decoration_v1 *m_Decoration = nullptr;
-  bool using_csd = false;
-  bool configured = false;
-#ifdef HAVE_LIBDECOR
-  libdecor *context;
-  libdecor_frame *frame;
-  int content_width;
-  int content_height;
-  int floating_width;
-  int floating_height;
-  bool open = false;
-#endif
-
+  // wlr layer shell
+  zwlr_layer_shell_v1 *m_WlrLayerShell = nullptr;
+  zwlr_layer_surface_v1 *layer_surface = nullptr;
+  // egl
+  wl_egl_window *m_EGLWindow = nullptr;
   EGLDisplay m_EGLDisplay = nullptr;
   EGLContext m_EGLContext = nullptr;
   EGLSurface m_EGLSurface = nullptr;
   EGLConfig m_EGLConfig = nullptr;
+
+#ifdef HAVE_LIBDECOR
+  libdecor *m_LibdecorContext;
+  libdecor_frame *m_LibdecorFrame;
+  int m_LibdecorContentWidth;
+  int m_LibdecorContentHeight;
+  int m_LibdecorFloatingWidth;
+  int m_LibdecorFloatingHeight;
+  bool open = false;
+#endif
 
   bool m_FullScreen;
   bool m_Background = false;
@@ -69,6 +72,157 @@ class CWaylandGL : public CDisplayOutput {
 
   void setFullScreen(bool enabled);
   void checkClientMessages();
+
+// wayland handling stuff starts here
+#ifdef HAVE_LIBDECOR
+  static void frame_configure(struct libdecor_frame *frame,
+                              struct libdecor_configuration *configuration,
+                              void *user_data) {
+    CWaylandGL *waylandGL = static_cast<CWaylandGL *>(user_data);
+    struct libdecor_state *state;
+    int width, height;
+
+    if (!libdecor_configuration_get_content_size(configuration, frame, &width,
+                                                 &height)) {
+      width = waylandGL->m_LibdecorFloatingWidth;
+      height = waylandGL->m_LibdecorFloatingHeight;
+    }
+
+    waylandGL->m_LibdecorContentWidth = width;
+    waylandGL->m_LibdecorContentHeight = height;
+
+    wl_egl_window_resize(waylandGL->m_EGLWindow,
+                         waylandGL->m_LibdecorContentWidth,
+                         waylandGL->m_LibdecorContentHeight, 0, 0);
+    glViewport(0, 0, waylandGL->m_LibdecorContentWidth,
+               waylandGL->m_LibdecorContentHeight);
+
+    state = libdecor_state_new(width, height);
+    libdecor_frame_commit(frame, state, configuration);
+    libdecor_state_free(state);
+
+    /* store floating dimensions */
+    if (libdecor_frame_is_floating(waylandGL->m_LibdecorFrame)) {
+      waylandGL->m_LibdecorFloatingWidth = width;
+      waylandGL->m_LibdecorFloatingHeight = height;
+    }
+
+    waylandGL->configured = true;
+  }
+
+  static void frame_close(struct libdecor_frame *frame, void *user_data) {
+    CWaylandGL *waylandGL = static_cast<CWaylandGL *>(user_data);
+
+    waylandGL->m_bClosed = true;
+  }
+
+  static void frame_commit(struct libdecor_frame *frame, void *user_data) {
+    CWaylandGL *waylandGL = static_cast<CWaylandGL *>(user_data);
+    eglSwapBuffers(waylandGL->m_EGLDisplay, waylandGL->m_EGLSurface);
+  }
+
+  struct libdecor_frame_interface frame_interface = {
+      frame_configure,
+      frame_close,
+      frame_commit,
+  };
+
+  static void libdecor_error(struct libdecor *context,
+                             enum libdecor_error error, const char *message) {
+    fprintf(stderr, "Caught error (%d): %s\n", error, message);
+    exit(EXIT_FAILURE);
+  }
+
+  struct libdecor_interface libdecor_interface = {
+      libdecor_error,
+  };
+#endif
+
+  static void xdg_wm_base_ping(void *data, struct xdg_wm_base *xdg_wm_base,
+                               uint32_t serial) {
+    xdg_wm_base_pong(xdg_wm_base, serial);
+  }
+
+  const struct xdg_wm_base_listener xdg_wm_base_listener = {
+      .ping = xdg_wm_base_ping,
+  };
+
+  static void xdg_toplevel_configure_handler(void *data,
+                                             struct xdg_toplevel *xdg_toplevel,
+                                             int32_t width, int32_t height,
+                                             struct wl_array *states) {
+    fprintf(stderr, "configure: %dx%d\n", width, height);
+    CWaylandGL *waylandGL = static_cast<CWaylandGL *>(data);
+    if (width == 0 || height == 0) {
+      /* Compositor is deferring to us */
+      return;
+    }
+    waylandGL->m_WidthFS = width;
+    waylandGL->m_HeightFS = height;
+    glViewport(0, 0, width, height);
+  }
+
+  static void xdg_toplevel_close_handler(void *data,
+                                         struct xdg_toplevel *xdg_toplevel) {
+    CWaylandGL *waylandGL = static_cast<CWaylandGL *>(data);
+    waylandGL->m_bClosed = true;
+  }
+
+  const struct xdg_toplevel_listener xdg_toplevel_listener = {
+      .configure = xdg_toplevel_configure_handler,
+      .close = xdg_toplevel_close_handler};
+
+  static void xdg_surface_configure_handler(void *data,
+                                            struct xdg_surface *xdg_surface,
+                                            uint32_t serial) {
+    CWaylandGL *waylandGL = static_cast<CWaylandGL *>(data);
+    xdg_surface_ack_configure(xdg_surface, serial);
+    waylandGL->configured = true;
+  }
+
+  const struct xdg_surface_listener xdg_surface_listener = {
+      .configure = xdg_surface_configure_handler};
+
+  static void zwlr_layer_surface_configure_handler(
+      void *data, struct zwlr_layer_surface_v1 *zwlr_layer_surface_v1,
+      uint32_t serial, uint32_t width, uint32_t height) {
+    glViewport(0, 0, width, height);
+    zwlr_layer_surface_v1_ack_configure(zwlr_layer_surface_v1, serial);
+  }
+
+  const struct zwlr_layer_surface_v1_listener layer_surface_listener = {
+      .configure = zwlr_layer_surface_configure_handler};
+
+  static void registry_handle_global(void *data, struct wl_registry *registry,
+                                     uint32_t name, const char *interface,
+                                     uint32_t version) {
+    CWaylandGL *waylandGL = static_cast<CWaylandGL *>(data);
+    // fprintf(stderr, "Got a registry event for %s id %d\n", interface, name);
+    if (strcmp(interface, wl_compositor_interface.name) == 0) {
+      waylandGL->m_Compositor = (struct wl_compositor *)wl_registry_bind(
+          registry, name, &wl_compositor_interface, 1);
+    } else if (strcmp(interface, xdg_wm_base_interface.name) == 0) {
+      waylandGL->m_XdgWmBase = (struct xdg_wm_base *)wl_registry_bind(
+          registry, name, &xdg_wm_base_interface, 1);
+      xdg_wm_base_add_listener(waylandGL->m_XdgWmBase,
+                               &waylandGL->xdg_wm_base_listener, waylandGL);
+    } else if (strcmp(interface, wl_output_interface.name) == 0) {
+      waylandGL->m_Output = (struct wl_output *)wl_registry_bind(
+          registry, name, &wl_output_interface, 1);
+    } else if (strcmp(interface, zwlr_layer_shell_v1_interface.name) == 0) {
+      waylandGL->m_WlrLayerShell =
+          (struct zwlr_layer_shell_v1 *)wl_registry_bind(
+              registry, name, &zwlr_layer_shell_v1_interface, 1);
+    } else if (strcmp(interface, zxdg_decoration_manager_v1_interface.name) ==
+               0) {
+      waylandGL->m_DecorationManager =
+          (struct zxdg_decoration_manager_v1 *)wl_registry_bind(
+              registry, name, &zxdg_decoration_manager_v1_interface, 1);
+    }
+  }
+  const struct wl_registry_listener registry_listener = {registry_handle_global,
+                                                         nullptr};
+  // wayland handling stuff ends here
 
 public:
   CWaylandGL();
@@ -81,27 +235,8 @@ public:
   virtual void Title(const std::string &_title);
   virtual void Update();
 
-  void request_next_frame();
+  // void request_next_frame();
   void SwapBuffers();
-
-  friend void registry_handle_global(void *data, struct wl_registry *registry,
-                                     uint32_t name, const char *interface,
-                                     uint32_t version);
-  friend void xdg_toplevel_configure_handler(void *data,
-                                             struct xdg_toplevel *xdg_toplevel,
-                                             int32_t width, int32_t height,
-                                             struct wl_array *states);
-  friend void xdg_toplevel_close_handler(void *data,
-                                         struct xdg_toplevel *xdg_toplevel);
-  friend void xdg_surface_configure_handler(void *data,
-                                            struct xdg_surface *xdg_surface,
-                                            uint32_t serial);
-  friend void frame_configure(struct libdecor_frame *frame,
-                              struct libdecor_configuration *configuration,
-                              void *user_data);
-  friend void frame_close(struct libdecor_frame *frame, void *user_data);
-  friend void frame_destroy(struct libdecor_frame *frame, void *user_data);
-  friend void frame_commit(struct libdecor_frame *frame, void *user_data);
 };
 
 #if defined(HAVE_WAYLAND) && !defined(HAVE_X11)
